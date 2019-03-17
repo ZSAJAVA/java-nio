9531b1e050811fea17bae33a6c53d4bd4c190f5e
an_glb <an_glb@qq.com>
2019-03-17 18:23:33 +0800

    first2

--- count/src/main/java/com/xcu/count/Count.java
+++ count/src/main/java/com/xcu/count/Count.java
@@ -78,7 +78,7 @@ public class Count {
         //字符数
         int length = sb.toString().length();
 
-        System.out.println("字符数："+length);
+        System.out.println("字符个数："+length);
 
 
         br.close();
